dbc6b818b50a8070adff85a0eec2b724fee85ba5
Azan-Shah <69967451+Azan-Shah@users.noreply.github.com>
2020-12-17 02:03:13 +0800

    Update bot.js
    add new required package "round-to".
    remove unused prefix const.
    add brand new function "usage" to check resource consumption of the minecraft server.
    clean up update function code and add a new feature to check the status of minecraft server.
    now it will check minecraft server status on bot startup.
    and finally, add a comment to understand it better in the future.

--- bot.js
+++ bot.js
@@ -1,52 +1,80 @@
 const Discord = require('discord.js');
 const { resolve } = require('path');
 const config = require('./config.json');
+const roundTo = require('round-to');
 const client = new Discord.Client();
-const prefix = '!';
 const api = require("multicraft").begin({
     url: config.url,
     user: config.user,
     key: config.key
 });
 
+async function usage() {
+    const response = await api.getServerResources(config.serverid);
+    const profile = JSON.parse(response);
+    const cpu = profile.data.cpu;
+    var temp = Number(profile.data.memory);
+    const ram = roundTo(temp, 0);
+    client.user.setActivity(`CPU:${cpu}% RAM:${ram}0mb`, { type: "WATCHING"});
+}
+
 async function update() {
-        const response = await api.getServerStatus(126741);
+        const response = await api.getServerStatus(config.serverid);
         const profile = JSON.parse(response);
+        const status = profile.data.status;
         const player = profile.data.onlinePlayers[0];
         const maxplayer = profile.data.maxPlayers;
-        const channelId = '728516758666870855';
-        const channel = client.channels.cache.get(channelId)
-        const channelname = channel.name;
+        const channelid_status = config.status_voice_channel;
+        const channelId_players = config.player_voice_channel;
+        const channel_info1 = client.channels.cache.get(channelid_status); // Get Info About Voice Channel
+        const channel_info2 = client.channels.cache.get(channelId_players); // Get Info About Voice Channel
+        const channelname2 = channel_info1.name; // Get Name from JSON response of Voice Channel Info
+        const channelname = channel_info2.name; // Get Name from JSON response of Voice Channel Info
 
-        if (player >= 1)
+        if  (status === `online`)  // check if the status of the server is online
         {
-            var nametemplate = `Online Players: ${player}/${maxplayer}`;
+             var statustemplate = `Status: ${status}`; // current status of the server
+             if  (channelname2 !== statustemplate)
+             {
+                channel2.setName(statustemplate);    // update channel name to the latest 
+             };   
+        if (player >= 1)  // check if players is more than 1
+        {
+            var nametemplate = `Online Players: ${player}/${maxplayer}`;  // current player of the server
             if  (channelname !== nametemplate)
             {
-                channel.setName(nametemplate);
-            }
-            else
-            {
-                console.log("Players More Than 0");
+                channel.setName(nametemplate);  // update channel name to the latest 
             };
         }
-        else if (player <= 0)
+        else if (player <= 0)  // check if players is less than 0
         {
             var defaulttemplate = `Online Players: 0/${maxplayer}`;
             if  (channelname !== defaulttemplate)
             {
-                channel.setName(defaulttemplate);
-            }
-            else
+                channel.setName(defaulttemplate);    // update channel name to the latest 
+            };
+        };
+        }
+        else if (status === `offline`)       // check if the status of the server is offline
+        {
+            var offlinetemplate = `Status: ${status}`;
+            if  (channelname !== nametemplate)
             {
-                console.log("The Same Name/Value");
+                channel2.setName(offlinetemplate);   // update channel name to the latest 
             };
         };
 
 };
 
+
 client.on("ready", () => {
-    client.setInterval(update, 60000);
+    api.getServerStatus(config.serverid).then((response) => {
+        const profile = JSON.parse(response);
+        console.log("Minecraft Server is " + profile.data.status);
+      });   // check the status of minecraft server
+
+    client.setInterval(usage, 10000); // run usage function every 10 seconds
+    client.setInterval(update, 30000); // run update function every 30 seconds
 });
 
 process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
